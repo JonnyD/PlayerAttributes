397cc6084fdc862115ce9cb362223294a15ee323
JonnyD <contact@jonnydevine.com>
2013-04-03 19:48:38 +0100

    Get or create civPlayer upon logging in to server

--- src/playerattributes/listener/PlayerListener.java
+++ src/playerattributes/listener/PlayerListener.java
@@ -4,21 +4,22 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import playerattributes.PlayerAttributes;
+import playerattributes.manager.CivPlayerManager;
+
 public class PlayerListener implements Listener
 {
-
-	private int totalDistanceRan = 0;
-	private Location start;
-	private Location finish;
-	
 	@EventHandler
-	public void toggleSprint(PlayerToggleSprintEvent event)
+	public void login(PlayerLoginEvent event)
 	{
-		
+		Player player = event.getPlayer();
+		CivPlayerManager civPlayerManager = PlayerAttributes.getCivPlayerManager();
+		civPlayerManager.getOrCreateCivPlayer(player);
 	}
 }
