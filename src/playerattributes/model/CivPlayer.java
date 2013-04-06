cbf5602bab3ba05b4615ce47c32ed6ce161043fa
JonnyD <contact@jonnydevine.com>
2013-04-06 13:44:46 +0100

    Added toString

--- src/playerattributes/model/CivPlayer.java
+++ src/playerattributes/model/CivPlayer.java
@@ -4,11 +4,64 @@ import org.bukkit.entity.Player;
 
 public class CivPlayer 
 {
-	
 	private final Player player;
+	private int speed;
+	private double stamina;
+	private double bodyfat;
+	private double muscle;
 
 	public CivPlayer(Player player) 
 	{
 		this.player = player;
 	}
+	
+	public String getUsername()
+	{
+		return player.getDisplayName();
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public double getStamina() {
+		return stamina;
+	}
+
+	public void setStamina(double stamina) {
+		this.stamina = stamina;
+	}
+
+	public double getBodyfat() {
+		return bodyfat;
+	}
+
+	public void setBodyfat(double bodyfat) {
+		this.bodyfat = bodyfat;
+	}
+
+	public double getMuscle() {
+		return muscle;
+	}
+
+	public void setMuscle(double muscle) {
+		this.muscle = muscle;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	@Override
+	public String toString() {
+		return "CivPlayer [getUsername()=" + getUsername() + ", getSpeed()="
+				+ getSpeed() + ", getStamina()=" + getStamina()
+				+ ", getBodyfat()=" + getBodyfat() + ", getMuscle()="
+				+ getMuscle() + ", getPlayer()=" + getPlayer() + "]";
+	}
+
 }
